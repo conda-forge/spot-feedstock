4281be76993c4dc281cb8860c5296578fbd77a2e
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2024-06-19 11:24:48 +0200

    disable test suite on aarch64 and ppc64le
    it's too slow

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,13 @@ set -e -x # abort on error
 # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 # for an explanation of -D_LIBCPP_DISABLE_AVAILABILITY
 ./configure --prefix=$PREFIX --disable-devel --enable-optimizations CPPFLAGS=-D_LIBCPP_DISABLE_AVAILABILITY
-make
-make check || { cat buddy/src/test-suite.log; cat tests/test-suite.log; exit 1; }
+make -j2
+# We use travis to build aarch64/ppc64le builds, but the time limit is to small for the test suite
+case $BUILD in
+    *aarch64*) ;;
+    *ppc64le*) ;;
+    *) make check || { cat buddy/src/test-suite.log; cat tests/test-suite.log; exit 1; } ;;
+esac
 make install-strip
 mkdir -p $PREFIX/share/doc/spot/examples
 cp tests/python/[a-z]*.ipynb $PREFIX/share/doc/spot/examples
